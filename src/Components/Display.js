640278db382e85e685e39d60a54631ad879a1ab5
Myra Smith <61550209+myrasmith@users.noreply.github.com>
2021-05-13 15:00:14 -0400

    Update Display.js

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -6,12 +6,17 @@ constructor() {
   super()
  this.commaFormater = new Intl.NumberFormat("en");
 }
+  
+  // You could also create commaFormater here, e.g. :
+  // commaFormater = new Intl.NumberFormat("en")
+  
   render() {
     const { calculatorScreenValue } = this.props
     return (
       <div className="display">
         <input 
         type="text" 
+        {/* I love that you are formatting the display number in your render logic - way to think in react! */}
         value={this.commaFormater.format(calculatorScreenValue)}
         >
         </input>
